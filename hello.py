65de7bf8f6540af66402aae9bc8a95af96f1e5ef
inversecrime <146124212+inversecrime@users.noreply.github.com>
2024-01-09 00:30:48 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,14 @@ print("hi")
 import numpy
 print(numpy.__version__)
 numpy.random.randn()
+print("hi")
+import numpy
+print(numpy.__version__)
+numpy.random.randn()
+import platform
+print(platform.machine())
+print(platform.version())
+print(platform.platform())
+print(platform.uname())
+print(platform.system())
+print(platform.processor())
